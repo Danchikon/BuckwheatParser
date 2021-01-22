4730a638e94564541846a573f1c2b9600ad64e8e
DenisTvardovskiy <40178797+DenisTvardovskiy@users.noreply.github.com>
2021-01-22 16:14:09 +0200

    prom parser

--- parsers/prom.py
+++ parsers/prom.py
@@ -1,48 +1,46 @@
 from bs4 import BeautifulSoup
 from scripts import *
 from _settings import *
-from colorama import Fore
 
 
-class MetroParser:
+class PromParser:
     def parse(self):
         buckwheats = []
 
-        # pages = self.getPagesCount()
-        pages = 1
-        print(' - count of pages:', pages)
+        pages = self.getPagesCount()
+        print('count of pages:', pages)
         for page in range(1, pages + 1):
-                print(' - page:', page)
-                page = getPage(METRO_URL + f'?page={page}')
+                print('page:', page)
+                page = getPage(PROM_URL + f'&page={page}')
+                
 
                 try:
                     soup = BeautifulSoup(page.text, MARKUP)
                     
-                    products = soup.find_all('div', class_='products-box__list-item')
+                    products = soup.find_all('div', class_='ek-grid__item')
                 except Exception as e:
-                    print(Fore.RED, 'error', e, Fore.RESET, sep=' | ')
+                    print('error', e, sep=' | ')
                 else:
                     extraBuckwheats = self.parseProducts(products)
                     buckwheats += extraBuckwheats
 
         return buckwheats
-
     def parseProducts(self, products):
         buckwheats = []
 
         for product in products:
             try:
-                title = product.find('a', class_='product-tile')
-                productHref = title['href']
+                title = product.find('a', attrs={"data-qaid": "product_link"})
+                productHref = "https://prom.ua"+title['href']
                 productName = title['title']
                 try:
-                    productPrice = float(product.find('span', class_='Price__value_caption').text)
+                    productPrice = float(product.find('span',attrs={"data-qaid": "product_price"}).text)
 
                 except Exception as e:
-                    print(Fore.RED, 'error', 'can not get a price', e, Fore.RESET, sep=' | ')
+                    print('error', 'can not get a price', e, sep=' | ')
                     productPrice = None
                 
-                productPage = getPage("https://metro.zakaz.ua"+productHref)
+                productPage = getPage(productHref)
                 soup = BeautifulSoup(productPage.text, MARKUP)
 
                 characteristics = soup\
@@ -59,34 +57,35 @@ class MetroParser:
                     else:
                         productWeight = float(productWeight[:index])
                 except Exception as e:
-                    print(Fore.RED, 'error', 'can not get a weight', e, Fore.RESET, sep=' | ')
+                    print('error', 'can not get a weight', e, sep=' | ')
 
 
                 productCountry = None
                 try:
                     productCountry = characteristicsDict['Країна походження']
                 except Exception as e:
-                    print(Fore.RED, 'error', 'can not get a country', e, Fore.RESET, sep=' | ')
+                    print('error', 'can not get a country', e, sep=' | ')
 
                 try:
                     price_g = productPrice / productWeight
                 except Exception as e:
-                    print(Fore.RED, 'error', e, Fore.RESET, sep=' | ')
+                    print('error', e, sep=' | ')
                     price_g = None
 
                 buckwheat = {
+                    
                     'name': productName,
                     'price': productPrice,
                     'price_g': price_g,
                     'weight': productWeight,
                     'country': productCountry,
                     'site': 'Metro',
-                    'link': 'https://metro.zakaz.ua' + productHref
+                    'link': productHref
                 }
 
                 buckwheats.append(buckwheat)
             except Exception as e:
-                print(Fore.RED, 'error', e, Fore.RESET, sep=' | ')
+                print('error', e, sep=' | ')
 
         return buckwheats
 
@@ -97,25 +96,20 @@ class MetroParser:
                 key = characteristic.find(class_='big-product-card__entry-title').text
                 value = characteristic.find(class_='big-product-card__entry-value').text
             except Exception as e:
-                print(Fore.RED, 'error', e, Fore.RESET, sep=' | ')
+                print('error', e, sep=' | ')
             else:
                 characteristicsDict.update([(key, value)])
 
         return characteristicsDict
-
-    def __str__(self):
-        return 'MetroParser'
     
     def getPagesCount(self):
-        page = getPage(METRO_URL)
+        page = getPage(PROM_URL)
         try:
             soup = BeautifulSoup(page.text, MARKUP)
-            pages = soup.find_all('a', class_='pagination__item')
+            pages = soup.find_all('button', class_='ek-button')
             count = int(pages[len(pages) - 1].text)
         except Exception as e:
             print('error', e, sep=' | ')
             return 1
         else:
             return count
-
-
