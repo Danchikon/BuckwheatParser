1cfa6081173c85986d130addc1a0c2a5004ceef7
Daniel <patikotmot@gmail.com>
2021-01-26 18:35:22 +0200

    fix list of parsers

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         print(Fore.RED, 'error', 'can not connect to the mongo client', e, Fore.RESET, sep=' | ')
     else:
         print(Fore.GREEN, '- connected successfully -', Fore.RESET)
-        parsers = [PromParser(), RozetkaParser(), PromParser()]  # [RozetkaParser(), MetroParser(), PromParser()]
+        parsers = [MetroParser(), RozetkaParser(), PromParser()]  # [RozetkaParser(), MetroParser(), PromParser()]
 
         buckwheatParser = BuckwheatParser(parsers, client.buckwheat)
         buckwheatParser.start()
