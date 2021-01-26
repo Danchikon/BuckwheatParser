f92a8b64d1c81d85f5a64c050d4f9196b98a7400
Daniel <patikotmot@gmail.com>
2021-01-26 18:58:07 +0200

    add timeout

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pymongo
 def main():
     try:
         colorama.init()
-        client = pymongo.MongoClient(MONGO_CONNECTION)
+        client = pymongo.MongoClient(MONGO_CONNECTION, connectTimeoutMS=9999999999, socketTimeoutMS=9999999999)
     except Exception as e:
         print(Fore.RED, 'error', 'can not connect to the mongo client', e, Fore.RESET, sep=' | ')
     else:
