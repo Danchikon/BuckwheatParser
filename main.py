f49605c46a83ae5d5697809111c360bc4ea103b2
Daniel <patikotmot@gmail.com>
2021-01-26 23:38:04 +0200

    .

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import pymongo
 def main():
     try:
         colorama.init()
-        client = pymongo.MongoClient(MONGO_CONNECTION, connectTimeoutMS=1000000, socketTimeoutMS=1000000, serverSelectionTimeoutMS=1000000)
+        client = pymongo.MongoClient(MONGO_CONNECTION, connectTimeoutMS=100000000, socketTimeoutMS=100000000, serverSelectionTimeoutMS=100000000)
     except Exception as e:
         print(Fore.RED, 'error', 'can not connect to the mongo client', e, Fore.RESET, sep=' | ')
     else:
         print(Fore.GREEN, '- connected successfully -', Fore.RESET)
-        parsers = [RozetkaParser(), ]  # [RozetkaParser(), MetroParser(), PromParser()]
+        parsers = [RozetkaParser(), MetroParser(), PromParser()]  # [RozetkaParser(), MetroParser(), PromParser()]
 
         buckwheatParser = BuckwheatParser(parsers, client.buckwheat)
         buckwheatParser.start()
